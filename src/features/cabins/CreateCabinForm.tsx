bcad48b219f372e25d4f8b5c678cb1166fcf77cf
Arijit Malakar <arijitmalakar1996@gmail.com>
2023-12-08 20:54:50 +0530

    Added create cabin form

---
+++ src/features/cabins/CreateCabinForm.tsx
@@ -0,0 +1,125 @@
+import { SubmitHandler, useForm } from "react-hook-form";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+
+import Input from "../../ui/Input";
+import Form from "../../ui/Form";
+import FormRow from "../../ui/FormRow";
+import Button from "../../ui/Button";
+import FileInput from "../../ui/FileInput";
+import Textarea from "../../ui/Textarea";
+import { createCabin } from "../../services/apiCabins";
+import { CabinType } from "./cabinType";
+
+function CreateCabinForm() {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    getValues,
+    formState: { errors },
+  } = useForm<CabinType>();
+
+  const queryClient = useQueryClient();
+
+  const { isPending: isCreating, mutate } = useMutation({
+    mutationFn: createCabin,
+    onSuccess: () => {
+      toast.success("New cabin successfully created");
+      queryClient.invalidateQueries({ queryKey: ["cabins"] });
+      reset();
+    },
+    onError: (err) => toast.error(err.message),
+  });
+
+  const onSubmit: SubmitHandler<CabinType> = (data) => {
+    mutate(data);
+  };
+
+  return (
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <FormRow label="Cabin name" error={errors?.name?.message}>
+        <Input
+          type="text"
+          id="name"
+          disabled={isCreating}
+          {...register("name", { required: "Cabin name is required" })}
+        />
+      </FormRow>
+
+      <FormRow label="Maximum capacity" error={errors?.maxCapacity?.message}>
+        <Input
+          type="number"
+          id="maxCapacity"
+          disabled={isCreating}
+          {...register("maxCapacity", {
+            required: "Max capacity is required",
+            min: {
+              value: 1,
+              message: "Capacity should be at least 1",
+            },
+          })}
+        />
+      </FormRow>
+
+      <FormRow label="Regular price" error={errors?.regularPrice?.message}>
+        <Input
+          type="number"
+          id="regularPrice"
+          disabled={isCreating}
+          {...register("regularPrice", {
+            required: "Cabin price is required",
+            min: {
+              value: 1,
+              message: "Price should be at least 1",
+            },
+          })}
+        />
+      </FormRow>
+
+      <FormRow label="Discount" error={errors?.discount?.message}>
+        <Input
+          type="number"
+          id="discount"
+          disabled={isCreating}
+          defaultValue={0}
+          {...register("discount", {
+            required: "Discount is required",
+            validate: (value) =>
+              Number(value) <= Number(getValues().regularPrice) ||
+              "Discount should be less than regular price",
+          })}
+        />
+      </FormRow>
+
+      <FormRow
+        label="Description for website"
+        error={errors?.description?.message}
+      >
+        <Textarea
+          id="description"
+          defaultValue=""
+          disabled={isCreating}
+          {...register("description", {
+            required: "Description for cabin is required",
+          })}
+        />
+      </FormRow>
+
+      <FormRow label="Cabin photo">
+        <FileInput id="image" accept="image/*" />
+      </FormRow>
+
+      <FormRow>
+        <>
+          <Button variation="secondary" type="reset">
+            Cancel
+          </Button>
+          <Button disabled={isCreating}>Add cabin</Button>
+        </>
+      </FormRow>
+    </Form>
+  );
+}
+
+export default CreateCabinForm;
--- src/services/apiCabins.ts
+++ src/services/apiCabins.ts
@@ -1,3 +1,5 @@
+import { CabinType } from "../features/cabins/cabinType";
+
 export const getCabins = async () => {
   const res = await fetch("/api/cabins");
 
@@ -9,14 +11,27 @@ export const getCabins = async () => {
   return body.data.cabins;
 };
 
+export const createCabin = async (newCabin: CabinType) => {
+  const res = await fetch("/api/cabins", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(newCabin),
+  });
+
+  if (!res.ok) {
+    throw new Error("Could not create cabin");
+  }
+
+  const body = await res.json();
+  return body.data;
+};
+
 export const deleteCabin = async (id: string) => {
   const res = await fetch(`/api/cabins/${id}`, { method: "DELETE" });
 
   if (!res.ok) {
     throw new Error("Unable to delete cabin");
   }
-
-  //   const body = await res.json();
-  //   console.log(body);
-  //   return body;
 };
