b2e70f1b6a94d4be790f36a6aec473cec01c960e
Arijit Malakar <arijitmalakar1996@gmail.com>
2024-01-15 19:37:17 +0530

    Added prod link in proxy server

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://the-wild-oasis-api.vercel.app/
-// http://localhost:3000
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:3000",
-      "/uploads": "http://localhost:3000",
+      "/api": "https://the-wild-oasis-api.onrender.com",
+      "/uploads": "https://the-wild-oasis-api.onrender.com",
     },
   },
   plugins: [react()],
